187d5b8b64e9494796a75489dbe99d7b81b9b008
Alex Ukhov <ukhovalexd@gmail.com>
2026-08-17 18:54:39 +0500

    feat: create initial portfolio structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import './App.css'
 
 function App() {
   return (
-    <>
-    <h1>Alexey Ukhov</h1>
-    </>
+    <main>
+      <h1>Alexey Ukhov</h1>
+      <p>Frontend Developer Portfolio</p>
+      <section>
+        <h2>Projects</h2>
+        <article>
+          <h3>Coming soon</h3>
+        </article>
+      </section>
+    </main>
   )
 }
 
